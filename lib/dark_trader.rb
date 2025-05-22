7d4d2be601f271e62e8d4df5f7e27ef6e94d452f
RosaBen <benchabane.rosa@outlook.com>
2025-05-22 16:37:32 +0200

    crypto almost done, need to transform prices into float

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -8,26 +8,57 @@ require 'net/http'
 
 Dotenv.load
 
+def retrieve_dataXpath(url)
+
+  devisesFinal = []
+  pricesFinal = []
+  devisesScraped = []
+  rows = []
+  page = Nokogiri::HTML(URI.open(url))
+  rows = page.xpath('//tbody//tr')
+  columnName = rows.xpath ('//td[3]')
+  names = columnName.xpath('//div//a//span//div//div//div//p')
+  prices = rows.xpath('//td[4]//div//span')
+
+  names.each do |name|
+    devisesScraped.push(name)
+  end
+  devisesScraped.each do |text|
+    devisesFinal.push(text.text.strip)
+  end
+  prices.each do |price|
+ pricesFinal.push(price.text.strip)
+  end
+
+  combinedData = Hash[devisesFinal.zip(pricesFinal)]
+
+  puts combinedData
+
+end
+
+retrieve_dataXpath("https://coinmarketcap.com/")
+
 # puts ENV["TEST_KEY"]
 # puts ENV["TEST_URL"]
 
-def apiRequest
-  url = ENV["DARK_TRADER_URL"]
-  api_key = ENV["DARK_KEY"]
-  headers = {
-    "Content-Type" => "application/json",
-    "Authorization" => "Bearer #{api_key}",
-  }
-  data ={
-    "id" => "1",
-    "slug" => "bitcoin",
-    "symbol" => "BTC"
-  }
+# def apiRequest
+#   url = ENV["DARK_TRADER_URL"]
+#   api_key = ENV["DARK_KEY"]
+#   headers = {
+#     "Content-Type" => "application/json",
+#     "Authorization" => "Bearer #{api_key}",
+#   }
+#   data ={
+#     "id" => "1",
+#     "slug" => "bitcoin",
+#     "symbol" => "BTC"
+#   }
 
-  response = HTTP.get(url, headers: headers, json: data)
-  response_body = response.body.to_s
+#   response = HTTP.get(url, headers: headers, json: data)
 
-  puts response_body
-end
+#   puts response
+# end
 
-apiRequest
+# apiRequest
+
+
